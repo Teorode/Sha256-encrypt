50fccf4df98eb1821340eb2621503332b23ce731
Teodor <theteodor6@gmail.com>
2024-10-14 19:31:28 +0200

    Final commit

---
+++ encrypt.py
@@ -0,0 +1,12 @@
+import hashlib
+
+sha256hash = hashlib.sha256()
+
+input = input("Co chcesz zamienić na hash sha256?")
+
+input_bytes = input.encode('utf-8')
+sha256hash.update(input_bytes)
+
+result = sha256hash.hexdigest()
+
+print(result)
